55a3098e02141e8f1598e19c8cc1b8f546cd7df6
zaidmohd1609 <mrnolast@asu.edu>
2026-07-10 15:42:11 -0700

    fix: dynamic footer copyright year

--- src/components/Home/Footer.tsx
+++ src/components/Home/Footer.tsx
@@ -22,7 +22,7 @@ export default function Footer(): JSX.Element {
           padding: "30px",
         }}
       >
-        &copy; 2025 TOMNET UTC
+        &copy; {new Date().getFullYear()} TOMNET UTC
       </span>
 
 
